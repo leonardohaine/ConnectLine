581c156b3e79ccf0ce189e53047922aa81bb0b55
diego <diego@diego>
2016-09-14 14:06:58 -0300

    Nova Classe - Postos

---
+++ src/main/java/br/com/jlab/model/Exame.java
@@ -0,0 +1,31 @@
+package br.com.jlab.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Exame {
+	
+	private Long id;
+	private String exame;
+	private String descricao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getExame() {
+		return exame;
+	}
+	public void setExame(String exame) {
+		this.exame = exame;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
